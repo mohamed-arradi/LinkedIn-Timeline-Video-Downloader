b89f546a8357a9acf5c3a67614e69632c890e183
Mohamed Arradi-Alaoui <mohamedarradi.alaoui@dnata.com>
2020-01-04 01:31:11 +0100

    Add search timeline

--- data/js/content-script.js
+++ data/js/content-script.js
@@ -1,9 +1,3 @@
-"use strict";
-
-browser.runtime.onMessage.addListener(request => {
-    addVideoLink();
-    return Promise.resolve({response: "success"});
-});
 
 // Small hack to avoid duplicate
 function cleanUpDuplicate(videoID, element) {
@@ -16,21 +10,26 @@ function cleanUpDuplicate(videoID, element) {
 function addVideoLink() {
 
     var feedCardClass = "relative ember-view";
-    var allVideos = Array.from(document.getElementsByClassName(feedCardClass));
+    var searchCardClassName = "search-content__result search-entity ember-view"
+    
+    var timelineClassElements = document.getElementsByClassName(feedCardClass);
+
+    var allVideos = Array();
+    if (timelineClassElements !== undefined) {
+        allVideos = Array.from(timelineClassElements);
+    } else {
+        allVideos = Array.from(document.getElementsByClassName(searchCardClassName));
 
         allVideos.forEach(element => {
-            if (element !== undefined &&
-                element.className == feedCardClass) {
-
+            if (element !== undefined) {
                 var data = Array.from(element.getElementsByTagName("artdeco-dropdown-content"));
-
                 data.forEach(subElement => {
-                    
+
                     var dropDown = subElement.getElementsByClassName("artdeco-dropdown__content-inner")[0];
                     var listOptions = dropDown.getElementsByTagName("li");
                     var videoID = "video_dwl";
                     var videoElements = element.querySelectorAll('[id^="vjs_video_"][id$="_html5_api"]');
-                    
+
                     if (listOptions !== undefined &&
                         videoElements !== undefined &&
                         videoElements.length > 0) {
@@ -43,7 +42,7 @@ function addVideoLink() {
                             var liHTML = li;
 
                             liHTML.classList.remove("option-save");
-                            
+
                             var elementIsNotPresent = dropDown.querySelector("#" + videoID) === null;
                             var numberElements = Array.from(dropDown.querySelectorAll("#" + videoID));
 
@@ -52,9 +51,9 @@ function addVideoLink() {
                                 liHTML.getElementsByTagName("span")[1].innerHTML = "It will open a new window to download it"
                                 liHTML.getElementsByTagName("li-icon")[0].innerHTML = ""
                                 liHTML.getElementsByTagName("li-icon")[0].innerHTML = "<li-icon aria-hidden=\"true\" type=\"ribbon-icon\" class=\"flex-shrink-zero mr2\"><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" class=\"svg-icon\" viewBox=\"0 0 20 20\"><path d=\"M17.927,5.828h-4.41l-1.929-1.961c-0.078-0.079-0.186-0.125-0.297-0.125H4.159c-0.229,0-0.417,0.188-0.417,0.417v1.669H2.073c-0.229,0-0.417,0.188-0.417,0.417v9.596c0,0.229,0.188,0.417,0.417,0.417h15.854c0.229,0,0.417-0.188,0.417-0.417V6.245C18.344,6.016,18.156,5.828,17.927,5.828 M4.577,4.577h6.539l1.231,1.251h-7.77V4.577z M17.51,15.424H2.491V6.663H17.51V15.424z\"></path></svg></li-icon>"
-                                
+
                                 var videoElements = element.querySelectorAll('[id^="vjs_video_"][id$="_html5_api"]');
-                               
+
                                 if (videoElements.length > 0) {
                                     var videoElemSrc = videoElements[0].src;
                                     var innerHTMLText = "<a href=" + videoElemSrc + ">Download Embedded Video</a>"
@@ -69,4 +68,5 @@ function addVideoLink() {
                 });
             }
         });
+    }
 }
